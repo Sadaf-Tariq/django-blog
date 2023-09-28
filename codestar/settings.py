6ca99bac412736cb8d5049e8ca973c1a8f136fa8
Sadaf Tariq <sadaf.t.tarique@gmail.com>
2023-09-28 16:48:22 +0000

    Update settings.py

--- codestar/settings.py
+++ codestar/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['django-blog-sadaf.herokuapp.com','localhost']
+ALLOWED_HOSTS = ['django-blog-sadaf-d52b9d49b2cc.herokuapp.com','localhost']
 
 
 # Application definition
